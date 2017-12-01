8c04f54c8ef18efcbec9f0b4a053dbea0303a91f
Samuel Pagenkopf <ssaammp@gmail.com>
2017-12-01 00:58:08 -0600

    cleaner actor initialization

--- main.lua
+++ main.lua
@@ -1,27 +1,30 @@
 local level = dofile "levels/test.lua"
-local actors = {}
+local actors
 local base_actor = {
     x=0, y=0, dx=0, dy=0,
     hitboxes = {},
 }
-function base_actor:init () end
+function base_actor:setpos (x, y)
+    self.x, self.y = x ,y
+end
+function base_actor:init(x, y) self:setpos(x, y) end
 function base_actor:physics ()
     self.x = self.x + self.dx
     self.y = self.y + self.dy
 end
-function base_actor:update () self:physics() end
+function base_actor:update () self:physics () end
 function base_actor:die () end
 function base_actor:draw () end
 
-local add_actor = function (name, x, y, ...)
+local add_actor = function (name, ...)
     local actor = dofile("actors/" .. name .. ".lua")
     setmetatable(actor, {__index = base_actor})
-    actor.x, actor.y = x, y
     actor:init(...)
     actors[#actors+1] = actor
 end
 
 local load_level = function (name)
+    actors = {}
     local level = dofile("levels/" .. name .. ".lua")
     for _,actor in ipairs(level.actors) do
         add_actor(unpack(actor))

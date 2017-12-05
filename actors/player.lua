db1d2d67511b745e0362c6edf6e5558c73d9d4e4
Samuel Pagenkopf <ssaammp@gmail.com>
2017-12-05 00:07:57 -0600

    fixes

--- actors/player.lua
+++ actors/player.lua
@@ -8,8 +8,9 @@ local player = {
     player = true,
     top = true,
     hitboxes = {{8.5, 7.5, 3}},
-    blink = -100,
 }
+local color = {135, 182, 195}
+
 function player:init(x, y)
     self:setpos(x, y)
     self.blink = 100
@@ -18,18 +19,16 @@ function player:init(x, y)
 end
 function player:draw(artist)
     if self.yolk_anim.name == "yolk" then
-        if self.blink > 0 then
-            self.blink = self.blink - 1
-            if self.blink == 0 then
-                self.blink = math.floor(math.random() * 200)
-                self.yolk_anim = self:new_anim("yolk_blink", blink_len)
-            end
+        self.blink = self.blink - 1
+        if self.blink == 0 then
+            self.blink = math.floor(math.random() * 200)
+            self.yolk_anim = self:new_anim("yolk_blink", blink_len)
         end
     end
     artist.draw_anim(self.white_anim, self.x-self.dx, self.y-self.dy)
     artist.draw_anim(self.yolk_anim, self.x, self.y)
 end
-local color = {135, 182, 195}
+
 function player:update(game)
     if self.yolk_anim.name == "yolk_die" then
         if self.yolk_anim.pos > dead_len then
@@ -50,8 +49,8 @@ function player:update(game)
         end
     else
         if game.buttons.a == 1 then
-            game.spawn("bullet", self.x+9, self.y+4.5+self.dy*2,
-                       self.dx/2+2, self.dy, color, true)
+            game.spawn("bullet", self.x+9, self.y+4.5+self.dy,
+                       self.dx+3, self.dy, color, true)
             self.yolk_anim = self:new_anim("yolk_shoot", shoot_len, false)
             self.white_anim = self:new_anim("white_shoot", shoot_len, false)
             self.blink = -100
